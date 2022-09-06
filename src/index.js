9ab902c584602729261b53aa0394a7d4c30106a4
avishkakavindu <avishkakavindud@gmail.com>
2022-09-06 14:53:29 +0530

    React components and prop validations

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+const App = (props) => {
+  return (
+    <h2 className="text-center">
+      hello react with JSX - {props.headerMessage}
+    </h2>
+  );
+};
+
+App.propTypes = {
+  headerMessage: PropTypes.string.isRequired,
+};
+
+App.defaultProps = {
+  headerMessage: 'hello world',
+};
 
 ReactDOM.render(
-  <h2 className="text-center"> hello react with JSX </h2>,
+  <App headerMessage="hello react" />,
   document.getElementById('root')
 );
